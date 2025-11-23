e571bcce73df3f5590a3d02074667a26852c9c3e
Aldrin1710 <a19201468@alumnos.uady.mx>
2025-11-23 16:01:04 -0600

    Integracion de las vistas en la nueva rama

---
+++ EBOGE/src/controlador/ControladorPantallaCarga.java
@@ -0,0 +1,125 @@
+package controlador;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
+
+
+/**
+ * Controlador para la pantalla de carga.
+ * Gestiona la barra de progreso y los consejos aleatorios.
+ */
+public class ControladorPantallaCarga implements Initializable{
+	
+	 private static final String RUTA_CURIOSIDADES = "/archivos/curiosidades.txt";
+     private static final double INCREMENTO_PORCENTAJE = 0.01;
+     private static final double PORCENTAJE_MAXIMO = 1.0;
+     private static final int PORCENTAJE_CONVERSOR = 100;
+     
+     
+    
+	 @FXML
+	 private ProgressBar barraCarga;
+
+	 @FXML
+	 private Label lblCargando;
+
+	 @FXML
+	 private Label lblDatosCuriosos;
+	 
+	 private double progresoActual = 0.0;	 
+	 private Timeline timelineBarra;	 
+	 private Timeline timelineConsejos;
+	    
+	 
+	 	 
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {		
+		cargarBarraProgreso();
+		cargarConsejos();
+	}
+	
+	public void cargarBarraProgreso() {
+		timelineBarra = new Timeline();
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(200), event -> {
+            
+        progresoActual += INCREMENTO_PORCENTAJE; //0.01 
+            
+        barraCarga.setProgress(progresoActual);
+           
+        lblCargando.setText((int)(progresoActual * PORCENTAJE_CONVERSOR) + "%");
+            
+        if (progresoActual >= PORCENTAJE_MAXIMO) {
+        	timelineBarra.stop();
+            timelineConsejos.stop();
+        	}
+        });
+        
+        timelineBarra.getKeyFrames().add(keyFrame);        
+        timelineBarra.setCycleCount(Timeline.INDEFINITE);       
+        timelineBarra.play();	
+	}
+		
+	public void cargarConsejos() {
+		List<String> listaCuriosidades = leerCuriosidades();
+		
+		if (listaCuriosidades.isEmpty()) {
+            return;
+        }
+		
+		timelineConsejos = new Timeline();
+		KeyFrame keyframe = new KeyFrame(Duration.millis(5000), event -> {			
+			Random random = new Random();
+		    int indiceAleatorio = random.nextInt(listaCuriosidades.size());	        
+		    lblDatosCuriosos.setText(listaCuriosidades.get(indiceAleatorio));	    				
+		}); 
+				
+		timelineConsejos.getKeyFrames().add(keyframe);		
+		timelineConsejos.setCycleCount(Timeline.INDEFINITE);
+		timelineConsejos.play();		
+	}
+	
+	public List<String> leerCuriosidades(){
+		List<String> listaCuriosidades = new ArrayList<String>();
+		
+		try (InputStream archivo = getClass().getResourceAsStream(RUTA_CURIOSIDADES)) {
+            
+            if (archivo == null) {
+                return Collections.emptyList(); 
+            }
+
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(archivo, StandardCharsets.UTF_8))) {
+                
+                String linea;
+                while ((linea = reader.readLine()) != null) {
+                    if (!linea.trim().isEmpty()) {
+                        listaCuriosidades.add(linea);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo de curiosidades: " + e.getMessage());
+            return Collections.emptyList();
+        }
+		
+		return listaCuriosidades;
+	}
+	
+}
--- EBOGE/src/controlador/ControladorPrincipal.java
+++ EBOGE/src/controlador/ControladorPrincipal.java
@@ -1,16 +1,32 @@
 package controlador;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import modelo.mapa.GeneradorDeMapa;
 import modelo.mapa.Mapa;
 
 public class ControladorPrincipal {
+	
+	private static final String RUTA_FUENTE_PIXEL = "/fuentes/PressStart2P-Regular.ttf";
+    private static final String RUTA_FUENTE_ICONOS = "/fuentes/Font Awesome 7 Free-Solid-900.otf"; 
+    
+    private static final String RUTA_VISTA_INICIO = "/vista/Inicio.fxml"; 
+    private static final String RUTA_VISTA_CONFIGURACION = "/vista/.fxml"; //Por implementar!!
+    private static final String RUTA_VISTA_CARGAR_PARTIDA = "/vista/PantallaCarga.fxml";
+    private static final String RUTA_VISTA_PARTIDA = "/vista/VentanaDePartida.fxml"; 
+    
+    
+    private static final String TITULO_VENTANA_PRINCIPAL = "EBOGE - Epic Board Game Evolution";
+    private static final String HINT_PANTALLA_COMPLETA = "";
+        
+    private static final double TAMANIO_FUENTE_BASE = 10.0; // necesario para cargar las fuentes
 
 	private Stage ventanaPrincipal;
 
@@ -22,11 +38,23 @@ public class ControladorPrincipal {
 
 		this.ventanaPrincipal = stage;
 
-		mostrarVentanaDePartida(ladosDelDado);
+		mostrarVentanaDeInicio();
+		configurarPantallaCompleta(this.ventanaPrincipal);
 	}
 
 	public void mostrarVentanaDeInicio() {
-
+		
+		try {
+            cargarFuentesGlobales();
+            inicializarInterfaz(ventanaPrincipal, RUTA_VISTA_INICIO, TITULO_VENTANA_PRINCIPAL);
+            
+            
+        } catch (Exception e) {
+            System.err.println("Error critico al iniciar la aplicación: " + e.getMessage());
+            e.printStackTrace();
+        }
+		
+		
 	}
 
 	public void mostrarVentanaDeConfiguracion() {
@@ -34,39 +62,94 @@ public class ControladorPrincipal {
 	}
 
 	public void mostrarVentanaDeCarga() {
-
+		try {
+            inicializarInterfaz(ventanaPrincipal, RUTA_VISTA_CARGAR_PARTIDA, TITULO_VENTANA_PRINCIPAL);           
+        } catch (Exception e) {
+            System.err.println("Error critico al iniciar la aplicación: " + e.getMessage());
+            e.printStackTrace();
+        }
+		
 	}
 
 	private void mostrarVentanaDePartida(int ladosDelDado) throws IOException {
 
-		FXMLLoader cargadorFXML = new FXMLLoader(getClass().getResource("/Vista/VentanaDePartida.fxml"));
+	    FXMLLoader loader = inicializarInterfaz(ventanaPrincipal, RUTA_VISTA_PARTIDA, "EBOGE - Partida");
 
-		Parent raiz = cargadorFXML.load();
-		ControladorVentanaDePartida controladorPartida = cargadorFXML.getController();
+	    ControladorVentanaDePartida controladorPartida = loader.getController();
 
-		Scene escena = new Scene(raiz);
+	    Platform.runLater(() -> {
+	        double anchoTablero = controladorPartida.getAnchoTablero();
+	        double altoTablero = controladorPartida.getAltoTablero();
 
-		ventanaPrincipal.setScene(escena);
-		ventanaPrincipal.setTitle("EBOGE - Partida");
-		ventanaPrincipal.setMaximized(true);
-		ventanaPrincipal.show();
+	        System.out.println("Ancho del Tablero: " + anchoTablero);
+	        System.out.println("Alto del Tablero: " + altoTablero);
 
-		Platform.runLater(() -> {
+	        GeneradorDeMapa generadorDeMapa = new GeneradorDeMapa();
+	        Mapa mapaGenerado = generadorDeMapa.generarMapa((int) anchoTablero, (int) altoTablero, ladosDelDado);
 
-			double anchoTablero = controladorPartida.getAnchoTablero();
-			double altoTablero = controladorPartida.getAltoTablero();
-
-			System.out.println("Ancho del Tablero: " + anchoTablero);
-			System.out.println("Alto del Tablero: " + altoTablero);
-
-			GeneradorDeMapa generadorDeMapa = new GeneradorDeMapa();
-			Mapa mapaGenerado = generadorDeMapa.generarMapa((int) anchoTablero, (int) altoTablero, ladosDelDado);
-
-			controladorPartida.dibujarTablero(mapaGenerado);
-		});
+	        controladorPartida.dibujarTablero(mapaGenerado);
+	    });
 	}
 
 	public void mostrarVentanaDeVictoria() {
 
 	}
+	
+	
+	/* Metodos para configurar la pantalla de inicio EBOGE */
+	private void cargarFuentesGlobales() {
+        cargarFuente(RUTA_FUENTE_PIXEL);
+        cargarFuente(RUTA_FUENTE_ICONOS);
+    }
+	
+	private void cargarFuente(String rutaRecurso) {
+        try (InputStream fontStream = getClass().getResourceAsStream(rutaRecurso)) {
+            if (fontStream != null) {
+                Font.loadFont(fontStream, TAMANIO_FUENTE_BASE);
+            } else {
+                System.err.println("Advertencia: No se pudo encontrar la fuente en " + rutaRecurso);
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el flujo de la fuente: " + rutaRecurso);
+        }
+    }
+	
+	private FXMLLoader inicializarInterfaz(Stage escenario, String rutaEscena, String titulo) throws IOException {
+	    // Validar recurso
+	    if (getClass().getResource(rutaEscena) == null) {
+	        throw new IOException("No se encontró el archivo FXML en: " + rutaEscena);
+	    }
+
+	    // Usamos la instancia new FXMLLoader() en lugar del estático .load()
+	    // para poder retornar el cargador y obtener el controlador después.
+	    FXMLLoader loader = new FXMLLoader(getClass().getResource(rutaEscena));
+	    Parent raiz = loader.load();
+	    
+	    Scene escena = new Scene(raiz);
+
+	    escenario.setTitle(titulo);
+	    escenario.setScene(escena);
+	    
+	    escenario.setResizable(false);
+	    
+	    escenario.setFullScreenExitHint(HINT_PANTALLA_COMPLETA);
+	    escenario.setFullScreen(true);
+	    
+	    
+	    escenario.show();
+
+	    return loader; 
+	}
+	
+	private void configurarPantallaCompleta(Stage escenario) {
+        escenario.focusedProperty().addListener((observable, valorAntiguo, valorNuevo) -> {
+            // Si valorNuevo es true (recuperó el foco)
+            if (Boolean.TRUE.equals(valorNuevo)) {
+                // Usamos runLater para asegurar que el cambio de UI ocurra en el hilo correcto
+                // y después de que el sistema operativo termine de procesar el cambio de foco.
+                Platform.runLater(() -> escenario.setFullScreen(true));
+            }
+        });
+    }
+	
 }
