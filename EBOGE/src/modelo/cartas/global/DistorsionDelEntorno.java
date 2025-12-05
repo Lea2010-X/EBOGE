522e26f50c4f82c98b7a3c5ccc00c1f167541f9c
carlos romero <a23216399@alumnos.uady.mx>
2025-12-04 19:36:04 -0600

    Muy pequeños cambios

--- EBOGE/src/modelo/cartas/global/DistorsionDelEntorno.java
+++ EBOGE/src/modelo/cartas/global/DistorsionDelEntorno.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.logging.Logger;
-
-import controlador.ControladorPrincipal;
 import modelo.Partida;
 import modelo.cartas.Carta;
 import modelo.cartas.EfectoSinObjetivo;
